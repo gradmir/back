3506ec3b31eba4b04958fb6584c30f5408b1cbfd
Radmir Gainutdinov <radmir.abiy@gmail.com>
2021-10-22 21:35:48 +0300

    removed secret key

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'yandexlyceum_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://kqmjcjomubuvyd:52f4b6cd242a446fefb3e45fe3de66845a178fda77561544dbc609526459b35a@ec2-35-171-171-27.compute-1.amazonaws.com:5432/d4qt4et60i3rlj'
+  
