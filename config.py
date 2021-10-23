e12a0e1b1111d15d8547c8e272c59fa912b94be5
Radmir Gainutdinov <radmir.abiy@gmail.com>
2021-10-23 08:25:58 +0300

    changed config var name

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'yandexlyceum_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['POSTGRES_DATABASE_URL']
